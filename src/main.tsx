fa92371bae0d6eaa2447e2081c2ec63b9f2fa2d6
theusmandev <theusmandeveloper@gmail.com>
2026-07-17 10:00:00 +0500

    fix # in path routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App";
 
@@ -9,8 +9,8 @@ if (!root) throw new Error("No #root element found");
 
 createRoot(root).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/meetusamaservices">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
